cb74ba4b39c998db00e2904686c0b4ed5b623902
Rishi Satish <66351854+red-lance@users.noreply.github.com>
2024-11-05 12:27:26 +0530

    Update Stock_Management.java

--- src/main/java/stockmanagement/Stock_Management.java
+++ src/main/java/stockmanagement/Stock_Management.java
@@ -2,82 +2,85 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 package stockmanagement;
+
 /**
- *
+ * Main class for Stock Management System
+ * 
  * @author rishi
  */
 import javax.swing.JOptionPane;
+
 public class Stock_Management {
-    
-    public static final int ERROR_INSUFFICIENT_QUANTITY = -1;
-    public static final int ERROR_NOT_FOUND = -3;
-    public static final int ERROR_EMPTY_PORTFOLIO = -4;
-    public static final int FUND_BUY_CONFLICT = -5;
-    public static final int STOCK_BUY_CONFLICT = -2;
-    public static final int INVALID_PRICE = -6;
-    public static final int INVALID_QUANTITY = -7;
-    public static final int EMPTY_NAME = -8;
-    public static final int EMPTY_SYMBOL = -9;
-    public static final int UNKNOWN_ERROR = -10;
-    public static final int SUCCESS = 0;
-    
-    public static Portfolio portfolio;
-    
+
+    public static final int ERROR_INSUFFICIENT_QUANTITY = -1; // Not enough shares to sell
+    public static final int ERROR_NOT_FOUND = -3; // Investment not found
+    public static final int ERROR_EMPTY_PORTFOLIO = -4; // No investments in the portfolio
+    public static final int FUND_BUY_CONFLICT = -5; // Duplicate stock symbol
+    public static final int STOCK_BUY_CONFLICT = -2; // Duplicate mutual fund symbol
+    public static final int INVALID_PRICE = -6; // Price can't be zero or negative
+    public static final int INVALID_QUANTITY = -7; // Quantity can't be zero or negative
+    public static final int EMPTY_NAME = -8; // Investment name can't be blank
+    public static final int EMPTY_SYMBOL = -9; // Investment symbol can't be blank
+    public static final int UNKNOWN_ERROR = -10; // Some error happened that we don't know about
+    public static final int SUCCESS = 0; // Everything worked fine
+
+    public static Portfolio portfolio; // This is where all the investments are kept
+
     /**
-     * The main method of the program
-     * @param args the command line arguments
+     * The main method where the program starts
+     * @param args command line arguments (not used here)
      */
     public static void main(String[] args) {
-        portfolio = new Portfolio();
-        MainGUI gui = new MainGUI();
-        gui.setSize(1200, 650);
-        gui.setLocationRelativeTo(null);
-        gui.setResizable(false);
-        gui.setVisible(true);
+        portfolio = new Portfolio(); // Create a new portfolio
+        MainGUI gui = new MainGUI(); // Set up the main GUI for the app
+        gui.setSize(1200, 650); // Set the size of the window
+        gui.setLocationRelativeTo(null); // Center the window on the screen
+        gui.setResizable(false); // Make the window not resizable
+        gui.setVisible(true); // Show the window
     }
-    
+
     /**
-     * This method is solely dedicated to be called from outside to 
-     * display appropriate error messages to the user
-     * @param errorCode the displayed error is based on this error code
+     * Gets a user-friendly error message based on an error code
+     * @param errorCode the code that determines which error message to show
+     * @return a friendly error message
      */
     public static String getError(int errorCode) {
-        switch(errorCode) {
+        switch (errorCode) {
             case ERROR_INSUFFICIENT_QUANTITY:
-                return ("You don't have enough quantity to sell");
+                return "You don't have enough quantity to sell.";
             case ERROR_NOT_FOUND:
-                return ("You can only sell an investment you own");
+                return "You can only sell an investment you own.";
             case ERROR_EMPTY_PORTFOLIO:
-                return ("Your portfolio has no active investments");
+                return "Your portfolio has no active investments.";
             case FUND_BUY_CONFLICT:
-                return ("A Stock with the same symbol already exists");                
+                return "A Stock with the same symbol already exists.";                
             case STOCK_BUY_CONFLICT:
-                return ("A Mutual Fund with the same symbol already exists");                
+                return "A Mutual Fund with the same symbol already exists.";                
             case INVALID_PRICE:
-                return ("Price cannot be zero or negative");                
+                return "Price cannot be zero or negative.";                
             case INVALID_QUANTITY:
-                return ("Quantity cannot be zero or negative");                
+                return "Quantity cannot be zero or negative.";                
             case EMPTY_NAME:
-                return ("Investment name cannot be empty");                
+                return "Investment name cannot be empty.";                
             case EMPTY_SYMBOL:
-                return ("Investment symbol cannot be empty");                
+                return "Investment symbol cannot be empty.";                
             case UNKNOWN_ERROR:
-                return ("An unknown error occured");                
+                return "An unknown error occurred.";                
         }
-        return null;
+        return null; // If no error code matches, return null
     }
-    
+
     /**
-     * Shows an error message dialog
-     * @param msg The message to display in dialog
+     * Displays an error message in a dialog box
+     * @param msg The message to show
      */
     public static void showErrorDialog(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Error", JOptionPane.ERROR_MESSAGE);
     }
-    
+
     /**
-     * Shows an information message dialog
-     * @param msg The message to display in dialog
+     * Displays an info message in a dialog box
+     * @param msg The message to show
      */
     public static void showInfoDialog(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Information", JOptionPane.INFORMATION_MESSAGE);
